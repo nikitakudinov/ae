5faaee69abf40afa969c3c3d9fe7f2e5cbdc2730
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2023-11-30 16:35:39 +0000

    Updating to latest FlutterFlow output.

--- lib/full_search_results/full_search_results_widget.dart
+++ lib/full_search_results/full_search_results_widget.dart
@@ -109,7 +109,7 @@ class _FullSearchResultsWidgetState extends State<FullSearchResultsWidget> {
         backgroundColor: FlutterFlowTheme.of(context).primaryBackground,
         appBar: AppBar(
           backgroundColor: FlutterFlowTheme.of(context).primary,
-          automaticallyImplyLeading: false,
+          automaticallyImplyLeading: true,
           title: Text(
             'Page Title',
             style: FlutterFlowTheme.of(context).headlineMedium.override(
