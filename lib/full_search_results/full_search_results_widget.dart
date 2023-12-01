c40a1cfd4f77d6b26aab1e556f0dbc0d05bf6a28
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2023-12-01 10:48:55 +0000

    Updating to latest FlutterFlow output.

--- lib/full_search_results/full_search_results_widget.dart
+++ lib/full_search_results/full_search_results_widget.dart
@@ -272,7 +272,7 @@ class _FullSearchResultsWidgetState extends State<FullSearchResultsWidget> {
                                                     children: [
                                                       Text(
                                                         dateTimeFormat(
-                                                          'yMMMd Hm',
+                                                          'Hm',
                                                           functions
                                                               .newCustomFunction3(
                                                                   resultsRowItem
