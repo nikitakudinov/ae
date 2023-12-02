d27055480e07da97d77b57e87c6eb9938a1b2a15
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2023-12-02 15:29:47 +0000

    Updating to latest FlutterFlow output.

--- lib/pages/full_search_results/full_search_results_widget.dart
+++ lib/pages/full_search_results/full_search_results_widget.dart
@@ -1077,6 +1077,32 @@ class _FullSearchResultsWidgetState extends State<FullSearchResultsWidget> {
                                                   ),
                                                 ],
                                               ),
+                                              Expanded(
+                                                child: Align(
+                                                  alignment:
+                                                      AlignmentDirectional(
+                                                          1.00, 0.00),
+                                                  child: FlutterFlowIconButton(
+                                                    borderColor:
+                                                        Colors.transparent,
+                                                    borderRadius: 20.0,
+                                                    borderWidth: 1.0,
+                                                    buttonSize: 40.0,
+                                                    icon: Icon(
+                                                      Icons.add_shopping_cart,
+                                                      color:
+                                                          FlutterFlowTheme.of(
+                                                                  context)
+                                                              .primaryText,
+                                                      size: 24.0,
+                                                    ),
+                                                    onPressed: () {
+                                                      print(
+                                                          'IconButton pressed ...');
+                                                    },
+                                                  ),
+                                                ),
+                                              ),
                                             ],
                                           ),
                                         );
