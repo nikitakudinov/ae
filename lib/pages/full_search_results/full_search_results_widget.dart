d2b400b0836868787cfd6d5a7213e89c041825ba
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2023-12-02 16:04:40 +0000

    Updating to latest FlutterFlow output.

--- lib/pages/full_search_results/full_search_results_widget.dart
+++ lib/pages/full_search_results/full_search_results_widget.dart
@@ -10,6 +10,7 @@ import 'package:expandable/expandable.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/scheduler.dart';
 import 'package:flutter/services.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:provider/provider.dart';
 import 'full_search_results_model.dart';
@@ -758,6 +759,9 @@ class _FullSearchResultsWidgetState extends State<FullSearchResultsWidget> {
                                                   ExpandablePanelHeaderAlignment
                                                       .center,
                                               hasIcon: true,
+                                              expandIcon: Icons.add_rounded,
+                                              collapseIcon:
+                                                  FontAwesomeIcons.minus,
                                             ),
                                           ),
                                         ),
