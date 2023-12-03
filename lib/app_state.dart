c015c3b1d4ae1ad36dc4774965e450d0fe8166e3
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2023-12-03 17:54:37 +0000

    Updating to latest FlutterFlow output.

--- lib/app_state.dart
+++ lib/app_state.dart
@@ -142,7 +142,7 @@ class FFAppState extends ChangeNotifier {
     _searchRequests.insert(_index, _value);
   }
 
-  bool _SEARCHHISTORYvisibility = true;
+  bool _SEARCHHISTORYvisibility = false;
   bool get SEARCHHISTORYvisibility => _SEARCHHISTORYvisibility;
   set SEARCHHISTORYvisibility(bool _value) {
     _SEARCHHISTORYvisibility = _value;
