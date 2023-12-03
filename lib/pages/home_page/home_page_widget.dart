3fdcd8c5eaf43ecde7fd811ecaf39cc274d1ec63
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2023-12-03 16:57:23 +0000

    Updating to latest FlutterFlow output.

--- lib/pages/home_page/home_page_widget.dart
+++ lib/pages/home_page/home_page_widget.dart
@@ -83,186 +83,165 @@ class _HomePageWidgetState extends State<HomePageWidget> {
               child: Column(
                 mainAxisSize: MainAxisSize.max,
                 children: [
-                  TextFormField(
-                    controller: _model.textController,
-                    focusNode: _model.textFieldFocusNode,
-                    onChanged: (_) => EasyDebounce.debounce(
-                      '_model.textController',
-                      Duration(milliseconds: 2000),
-                      () async {
-                        _model.apiResultnpb = await AeGroup.brandsCall.call(
-                          code: _model.textController.text,
-                        );
-                        if ((_model.apiResultnpb?.succeeded ?? true)) {
-                          _model.data = await actions.jsonBrandCodeSearchResult(
-                            getJsonField(
-                              (_model.apiResultnpb?.jsonBody ?? ''),
-                              r'''$.DATA''',
-                              true,
-                            ),
-                          );
-                          setState(() {
-                            FFAppState().BrandCodeSearchResults = _model.data!
-                                .toList()
-                                .cast<BrandCodeSearchItemStruct>();
-                          });
-                        }
-
-                        setState(() {});
-                      },
-                    ),
-                    obscureText: false,
-                    decoration: InputDecoration(
-                      labelText: 'Артикул',
-                      labelStyle: FlutterFlowTheme.of(context).labelMedium,
-                      hintStyle: FlutterFlowTheme.of(context).labelMedium,
-                      enabledBorder: UnderlineInputBorder(
-                        borderSide: BorderSide(
-                          color: FlutterFlowTheme.of(context).alternate,
-                          width: 2.0,
-                        ),
-                        borderRadius: BorderRadius.circular(5.0),
-                      ),
-                      focusedBorder: UnderlineInputBorder(
-                        borderSide: BorderSide(
-                          color: FlutterFlowTheme.of(context).primary,
-                          width: 2.0,
-                        ),
-                        borderRadius: BorderRadius.circular(5.0),
-                      ),
-                      errorBorder: UnderlineInputBorder(
-                        borderSide: BorderSide(
-                          color: FlutterFlowTheme.of(context).error,
-                          width: 2.0,
-                        ),
-                        borderRadius: BorderRadius.circular(5.0),
-                      ),
-                      focusedErrorBorder: UnderlineInputBorder(
-                        borderSide: BorderSide(
-                          color: FlutterFlowTheme.of(context).error,
-                          width: 2.0,
-                        ),
-                        borderRadius: BorderRadius.circular(5.0),
-                      ),
-                    ),
-                    style: FlutterFlowTheme.of(context).bodyMedium,
-                    validator:
-                        _model.textControllerValidator.asValidator(context),
-                  ),
                   Container(
                     decoration: BoxDecoration(
-                      color: FlutterFlowTheme.of(context).secondaryBackground,
+                      color: Color(0xFFE4E4E4),
+                      borderRadius: BorderRadius.circular(5.0),
                     ),
-                    child: Padding(
-                      padding: EdgeInsetsDirectional.fromSTEB(
-                          10.0, 10.0, 10.0, 10.0),
-                      child: Builder(
-                        builder: (context) {
-                          final serchResults =
-                              FFAppState().BrandCodeSearchResults.toList();
-                          return ListView.builder(
-                            padding: EdgeInsets.zero,
-                            primary: false,
-                            shrinkWrap: true,
-                            scrollDirection: Axis.vertical,
-                            itemCount: serchResults.length,
-                            itemBuilder: (context, serchResultsIndex) {
-                              final serchResultsItem =
-                                  serchResults[serchResultsIndex];
-                              return InkWell(
-                                splashColor: Colors.transparent,
-                                focusColor: Colors.transparent,
-                                hoverColor: Colors.transparent,
-                                highlightColor: Colors.transparent,
-                                onTap: () async {
-                                  context.pushNamed(
-                                    'fullSearchResults',
-                                    queryParameters: {
-                                      'brand': serializeParam(
-                                        serchResultsItem.brand,
-                                        ParamType.String,
-                                      ),
-                                      'code': serializeParam(
-                                        serchResultsItem.code,
-                                        ParamType.String,
-                                      ),
-                                    }.withoutNulls,
-                                  );
-                                },
-                                child: Column(
-                                  mainAxisSize: MainAxisSize.max,
-                                  crossAxisAlignment: CrossAxisAlignment.start,
-                                  children: [
-                                    Text(
-                                      serchResultsItem.brand,
-                                      style: FlutterFlowTheme.of(context)
-                                          .bodyLarge,
-                                    ),
-                                    Text(
-                                      serchResultsItem.code,
-                                      style: FlutterFlowTheme.of(context)
-                                          .bodyMedium,
-                                    ),
-                                    Text(
-                                      serchResultsItem.name
-                                          .maybeHandleOverflow(maxChars: 100),
-                                      style: FlutterFlowTheme.of(context)
-                                          .bodySmall,
-                                    ),
-                                    Padding(
-                                      padding: EdgeInsetsDirectional.fromSTEB(
-                                          0.0, 5.0, 0.0, 5.0),
-                                      child: Container(
-                                        height: 1.0,
-                                        decoration: BoxDecoration(
-                                          color: Color(0xFFAEAEAE),
-                                        ),
-                                      ),
-                                    ),
-                                  ],
-                                ),
-                              );
-                            },
+                    child: TextFormField(
+                      controller: _model.textController,
+                      focusNode: _model.textFieldFocusNode,
+                      onChanged: (_) => EasyDebounce.debounce(
+                        '_model.textController',
+                        Duration(milliseconds: 2000),
+                        () async {
+                          _model.apiResultnpb = await AeGroup.brandsCall.call(
+                            code: _model.textController.text,
                           );
+                          if ((_model.apiResultnpb?.succeeded ?? true)) {
+                            _model.data =
+                                await actions.jsonBrandCodeSearchResult(
+                              getJsonField(
+                                (_model.apiResultnpb?.jsonBody ?? ''),
+                                r'''$.DATA''',
+                                true,
+                              ),
+                            );
+                            setState(() {
+                              FFAppState().BrandCodeSearchResults = _model.data!
+                                  .toList()
+                                  .cast<BrandCodeSearchItemStruct>();
+                            });
+                          }
+
+                          setState(() {});
                         },
                       ),
+                      obscureText: false,
+                      decoration: InputDecoration(
+                        labelText: 'Артикул',
+                        labelStyle: FlutterFlowTheme.of(context).labelMedium,
+                        hintStyle: FlutterFlowTheme.of(context).labelMedium,
+                        enabledBorder: UnderlineInputBorder(
+                          borderSide: BorderSide(
+                            color: FlutterFlowTheme.of(context).alternate,
+                            width: 2.0,
+                          ),
+                          borderRadius: BorderRadius.circular(5.0),
+                        ),
+                        focusedBorder: UnderlineInputBorder(
+                          borderSide: BorderSide(
+                            color: FlutterFlowTheme.of(context).primary,
+                            width: 2.0,
+                          ),
+                          borderRadius: BorderRadius.circular(5.0),
+                        ),
+                        errorBorder: UnderlineInputBorder(
+                          borderSide: BorderSide(
+                            color: FlutterFlowTheme.of(context).error,
+                            width: 2.0,
+                          ),
+                          borderRadius: BorderRadius.circular(5.0),
+                        ),
+                        focusedErrorBorder: UnderlineInputBorder(
+                          borderSide: BorderSide(
+                            color: FlutterFlowTheme.of(context).error,
+                            width: 2.0,
+                          ),
+                          borderRadius: BorderRadius.circular(5.0),
+                        ),
+                      ),
+                      style: FlutterFlowTheme.of(context).bodyMedium,
+                      validator:
+                          _model.textControllerValidator.asValidator(context),
                     ),
                   ),
-                  FFButtonWidget(
-                    onPressed: () async {
-                      context.pushNamed(
-                        'fullSearchResults',
-                        queryParameters: {
-                          'brand': serializeParam(
-                            'MAPCO',
-                            ParamType.String,
-                          ),
-                          'code': serializeParam(
-                            '5413',
-                            ParamType.String,
-                          ),
-                        }.withoutNulls,
-                      );
-                    },
-                    text: 'ЗАПРОС',
-                    options: FFButtonOptions(
-                      height: 40.0,
-                      padding:
-                          EdgeInsetsDirectional.fromSTEB(24.0, 0.0, 24.0, 0.0),
-                      iconPadding:
-                          EdgeInsetsDirectional.fromSTEB(0.0, 0.0, 0.0, 0.0),
-                      color: FlutterFlowTheme.of(context).primary,
-                      textStyle:
-                          FlutterFlowTheme.of(context).titleSmall.override(
-                                fontFamily: 'Asap Condensed',
-                                color: Colors.white,
-                              ),
-                      elevation: 3.0,
-                      borderSide: BorderSide(
-                        color: Colors.transparent,
-                        width: 1.0,
+                  Padding(
+                    padding:
+                        EdgeInsetsDirectional.fromSTEB(0.0, 10.0, 0.0, 0.0),
+                    child: Container(
+                      decoration: BoxDecoration(
+                        color: FlutterFlowTheme.of(context).secondaryBackground,
+                        borderRadius: BorderRadius.circular(5.0),
                       ),
-                      borderRadius: BorderRadius.circular(8.0),
+                      child: Padding(
+                        padding: EdgeInsetsDirectional.fromSTEB(
+                            10.0, 10.0, 10.0, 10.0),
+                        child: Builder(
+                          builder: (context) {
+                            final serchResults =
+                                FFAppState().BrandCodeSearchResults.toList();
+                            return ListView.builder(
+                              padding: EdgeInsets.zero,
+                              primary: false,
+                              shrinkWrap: true,
+                              scrollDirection: Axis.vertical,
+                              itemCount: serchResults.length,
+                              itemBuilder: (context, serchResultsIndex) {
+                                final serchResultsItem =
+                                    serchResults[serchResultsIndex];
+                                return InkWell(
+                                  splashColor: Colors.transparent,
+                                  focusColor: Colors.transparent,
+                                  hoverColor: Colors.transparent,
+                                  highlightColor: Colors.transparent,
+                                  onTap: () async {
+                                    context.pushNamed(
+                                      'fullSearchResults',
+                                      queryParameters: {
+                                        'brand': serializeParam(
+                                          serchResultsItem.brand,
+                                          ParamType.String,
+                                        ),
+                                        'code': serializeParam(
+                                          serchResultsItem.code,
+                                          ParamType.String,
+                                        ),
+                                      }.withoutNulls,
+                                    );
+                                  },
+                                  child: Column(
+                                    mainAxisSize: MainAxisSize.max,
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.start,
+                                    children: [
+                                      Text(
+                                        serchResultsItem.brand,
+                                        style: FlutterFlowTheme.of(context)
+                                            .titleMedium,
+                                      ),
+                                      Text(
+                                        serchResultsItem.code,
+                                        style: FlutterFlowTheme.of(context)
+                                            .labelLarge,
+                                      ),
+                                      Text(
+                                        serchResultsItem.name
+                                            .maybeHandleOverflow(maxChars: 100),
+                                        style: FlutterFlowTheme.of(context)
+                                            .labelSmall,
+                                      ),
+                                      Padding(
+                                        padding: EdgeInsetsDirectional.fromSTEB(
+                                            0.0, 5.0, 0.0, 5.0),
+                                        child: Container(
+                                          width:
+                                              MediaQuery.sizeOf(context).width *
+                                                  1.0,
+                                          height: 1.0,
+                                          decoration: BoxDecoration(
+                                            color: Color(0xFFDCDCDC),
+                                          ),
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                );
+                              },
+                            );
+                          },
+                        ),
+                      ),
                     ),
                   ),
                 ],
