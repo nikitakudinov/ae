39d1544699f5ce267f377c43493da02ff778fc45
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2023-12-05 17:51:04 +0000

    Updating to latest FlutterFlow output.

--- lib/components/stock_row_model.dart
+++ lib/components/stock_row_model.dart
@@ -4,25 +4,17 @@ import '/flutter_flow/flutter_flow_theme.dart';
 import '/flutter_flow/flutter_flow_util.dart';
 import '/flutter_flow/custom_functions.dart' as functions;
 import 'stock_row_widget.dart' show StockRowWidget;
-import 'package:expandable/expandable.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:provider/provider.dart';
 
 class StockRowModel extends FlutterFlowModel<StockRowWidget> {
-  ///  State fields for stateful widgets in this component.
-
-  // State field(s) for Expandable widget.
-  late ExpandableController expandableController;
-
   /// Initialization and disposal methods.
 
   void initState(BuildContext context) {}
 
-  void dispose() {
-    expandableController.dispose();
-  }
+  void dispose() {}
 
   /// Action blocks are added here.
 
--- lib/components/stock_row_widget.dart
+++ lib/components/stock_row_widget.dart
@@ -3,7 +3,6 @@ import '/flutter_flow/flutter_flow_icon_button.dart';
 import '/flutter_flow/flutter_flow_theme.dart';
 import '/flutter_flow/flutter_flow_util.dart';
 import '/flutter_flow/custom_functions.dart' as functions;
-import 'package:expandable/expandable.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:google_fonts/google_fonts.dart';
@@ -36,8 +35,6 @@ class _StockRowWidgetState extends State<StockRowWidget> {
   void initState() {
     super.initState();
     _model = createModel(context, () => StockRowModel());
-
-    _model.expandableController = ExpandableController(initialExpanded: false);
   }
 
   @override
@@ -120,8 +117,6 @@ class _StockRowWidgetState extends State<StockRowWidget> {
                               (e.stock == 1))
                           .toList()
                           .sortedList((e) => e.deliveryTime)
-                          .toList()
-                          .take(3)
                           .toList();
                       return ListView.builder(
                         padding: EdgeInsets.zero,
@@ -249,216 +244,6 @@ class _StockRowWidgetState extends State<StockRowWidget> {
                     },
                   ),
                 ),
-                if (FFAppState()
-                        .sr
-                        .where((e) =>
-                            ('\"${e.brand}\"' == widget.parameter1) &&
-                            (e.stock == 1))
-                        .toList()
-                        .sortedList((e) => e.deliveryTime)
-                        .length >
-                    3)
-                  Container(
-                    decoration: BoxDecoration(
-                      color: Color(0xFFF1F1F1),
-                      borderRadius: BorderRadius.only(
-                        bottomLeft: Radius.circular(5.0),
-                        bottomRight: Radius.circular(5.0),
-                        topLeft: Radius.circular(0.0),
-                        topRight: Radius.circular(0.0),
-                      ),
-                    ),
-                    child: Container(
-                      width: double.infinity,
-                      color: Color(0x00000000),
-                      child: ExpandableNotifier(
-                        controller: _model.expandableController,
-                        child: ExpandablePanel(
-                          header: Padding(
-                            padding: EdgeInsetsDirectional.fromSTEB(
-                                0.0, 0.0, 0.0, 10.0),
-                            child: Text(
-                              'В наличии еще на ${FFAppState().sr.where((e) => (e.cross == null || e.cross == '') && (e.stock == 1)).toList().length.toString()} складах Авто Евро',
-                              style: FlutterFlowTheme.of(context)
-                                  .labelSmall
-                                  .override(
-                                    fontFamily: 'Roboto Condensed',
-                                    color: FlutterFlowTheme.of(context)
-                                        .secondaryText,
-                                    fontSize: 12.0,
-                                    fontWeight: FontWeight.bold,
-                                  ),
-                            ),
-                          ),
-                          collapsed: Container(),
-                          expanded: Builder(
-                            builder: (context) {
-                              final cross02 = FFAppState()
-                                  .sr
-                                  .where((e) =>
-                                      ('\"${e.brand}\"' == widget.parameter1) &&
-                                      (e.stock == 1))
-                                  .toList()
-                                  .sortedList((e) => e.deliveryTime)
-                                  .toList();
-                              return ListView.builder(
-                                padding: EdgeInsets.zero,
-                                primary: false,
-                                shrinkWrap: true,
-                                scrollDirection: Axis.vertical,
-                                itemCount: cross02.length,
-                                itemBuilder: (context, cross02Index) {
-                                  final cross02Item = cross02[cross02Index];
-                                  return Row(
-                                    mainAxisSize: MainAxisSize.min,
-                                    mainAxisAlignment: MainAxisAlignment.start,
-                                    children: [
-                                      Expanded(
-                                        flex: 3,
-                                        child: Column(
-                                          mainAxisSize: MainAxisSize.max,
-                                          crossAxisAlignment:
-                                              CrossAxisAlignment.start,
-                                          children: [
-                                            Text(
-                                              cross02Item.warehouseName,
-                                              style:
-                                                  FlutterFlowTheme.of(context)
-                                                      .labelMedium,
-                                            ),
-                                          ],
-                                        ),
-                                      ),
-                                      Expanded(
-                                        child: Column(
-                                          mainAxisSize: MainAxisSize.max,
-                                          children: [
-                                            Text(
-                                              '${cross02Item.price.toString()}',
-                                              style:
-                                                  FlutterFlowTheme.of(context)
-                                                      .labelLarge,
-                                            ),
-                                            Text(
-                                              '${cross02Item.amount.toString()} ${cross02Item.unit}',
-                                              style:
-                                                  FlutterFlowTheme.of(context)
-                                                      .labelSmall,
-                                            ),
-                                          ],
-                                        ),
-                                      ),
-                                      Expanded(
-                                        child: Row(
-                                          mainAxisSize: MainAxisSize.max,
-                                          mainAxisAlignment:
-                                              MainAxisAlignment.center,
-                                          children: [
-                                            Text(
-                                              () {
-                                                if (dateTimeFormat(
-                                                      'yMd',
-                                                      functions
-                                                          .newCustomFunction3(
-                                                              cross02Item
-                                                                  .deliveryTime),
-                                                      locale:
-                                                          FFLocalizations.of(
-                                                                  context)
-                                                              .languageCode,
-                                                    ) ==
-                                                    dateTimeFormat(
-                                                      'yMd',
-                                                      getCurrentTimestamp,
-                                                      locale:
-                                                          FFLocalizations.of(
-                                                                  context)
-                                                              .languageCode,
-                                                    )) {
-                                                  return 'Сегодня';
-                                                } else if (dateTimeFormat(
-                                                      'yMd',
-                                                      functions
-                                                          .newCustomFunction3(
-                                                              cross02Item
-                                                                  .deliveryTime),
-                                                      locale:
-                                                          FFLocalizations.of(
-                                                                  context)
-                                                              .languageCode,
-                                                    ) ==
-                                                    dateTimeFormat(
-                                                      'yMd',
-                                                      functions.tomorrowdate(),
-                                                      locale:
-                                                          FFLocalizations.of(
-                                                                  context)
-                                                              .languageCode,
-                                                    )) {
-                                                  return 'Завтра';
-                                                } else {
-                                                  return dateTimeFormat(
-                                                    'relative',
-                                                    functions
-                                                        .newCustomFunction3(
-                                                            cross02Item
-                                                                .deliveryTime),
-                                                    locale: FFLocalizations.of(
-                                                                context)
-                                                            .languageShortCode ??
-                                                        FFLocalizations.of(
-                                                                context)
-                                                            .languageCode,
-                                                  );
-                                                }
-                                              }(),
-                                              style:
-                                                  FlutterFlowTheme.of(context)
-                                                      .labelSmall,
-                                            ),
-                                          ],
-                                        ),
-                                      ),
-                                      Expanded(
-                                        child: Align(
-                                          alignment:
-                                              AlignmentDirectional(1.00, 0.00),
-                                          child: FlutterFlowIconButton(
-                                            borderColor: Colors.transparent,
-                                            borderRadius: 20.0,
-                                            borderWidth: 1.0,
-                                            buttonSize: 40.0,
-                                            icon: Icon(
-                                              Icons.add_shopping_cart,
-                                              color:
-                                                  FlutterFlowTheme.of(context)
-                                                      .secondaryText,
-                                              size: 24.0,
-                                            ),
-                                            onPressed: () {
-                                              print('IconButton pressed ...');
-                                            },
-                                          ),
-                                        ),
-                                      ),
-                                    ],
-                                  );
-                                },
-                              );
-                            },
-                          ),
-                          theme: ExpandableThemeData(
-                            tapHeaderToExpand: true,
-                            tapBodyToExpand: false,
-                            tapBodyToCollapse: false,
-                            headerAlignment:
-                                ExpandablePanelHeaderAlignment.center,
-                            hasIcon: false,
-                          ),
-                        ),
-                      ),
-                    ),
-                  ),
               ],
             ),
           ),
