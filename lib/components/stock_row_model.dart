433df5289b13117c111dbfe46609007658b6703f
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2023-12-05 17:18:43 +0000

    Updating to latest FlutterFlow output.

--- lib/components/stock_row_model.dart
+++ lib/components/stock_row_model.dart
@@ -1,19 +1,15 @@
 import '/backend/schema/structs/index.dart';
-import '/components/stock_row_widget.dart';
 import '/flutter_flow/flutter_flow_icon_button.dart';
 import '/flutter_flow/flutter_flow_theme.dart';
 import '/flutter_flow/flutter_flow_util.dart';
 import '/flutter_flow/custom_functions.dart' as functions;
-import 'cross_item_widget.dart' show CrossItemWidget;
-import 'package:expandable/expandable.dart';
+import 'stock_row_widget.dart' show StockRowWidget;
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:percent_indicator/percent_indicator.dart';
 import 'package:provider/provider.dart';
 
-class CrossItemModel extends FlutterFlowModel<CrossItemWidget> {
+class StockRowModel extends FlutterFlowModel<StockRowWidget> {
   /// Initialization and disposal methods.
 
   void initState(BuildContext context) {}
