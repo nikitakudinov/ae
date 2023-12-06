c2415636fd4a06a6eb21d3edd3856c6601d96afe
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2023-12-06 16:16:19 +0000

    Updating to latest FlutterFlow output.

--- lib/components/minprice_widget.dart
+++ lib/components/minprice_widget.dart
@@ -52,33 +52,45 @@ class _MinpriceWidgetState extends State<MinpriceWidget> {
       decoration: BoxDecoration(
         color: FlutterFlowTheme.of(context).secondaryBackground,
       ),
-      child: Builder(
-        builder: (context) {
-          final codeItems = FFAppState()
-              .sr
-              .where((e) =>
-                  ('\"${e.code}\"' == widget.brand) &&
-                  ('\"${e.brand}\"' == widget.code))
-              .toList()
-              .sortedList((e) => e.price)
-              .toList()
-              .take(1)
-              .toList();
-          return ListView.builder(
-            padding: EdgeInsets.zero,
-            primary: false,
-            shrinkWrap: true,
-            scrollDirection: Axis.vertical,
-            itemCount: codeItems.length,
-            itemBuilder: (context, codeItemsIndex) {
-              final codeItemsItem = codeItems[codeItemsIndex];
-              return Text(
-                'БАЗОВЫЙ ЗАПРОС${codeItemsItem.code} ${codeItemsItem.brand}',
-                style: FlutterFlowTheme.of(context).bodyMedium,
+      child: Column(
+        mainAxisSize: MainAxisSize.max,
+        children: [
+          Align(
+            alignment: AlignmentDirectional(-1.00, 0.00),
+            child: Text(
+              'НАЛИЧИЕ',
+              style: FlutterFlowTheme.of(context).bodyMedium,
+            ),
+          ),
+          Builder(
+            builder: (context) {
+              final codeItems = FFAppState()
+                  .sr
+                  .where((e) =>
+                      ('\"${e.code}\"' == widget.brand) &&
+                      ('\"${e.brand}\"' == widget.code))
+                  .toList()
+                  .sortedList((e) => e.price)
+                  .toList()
+                  .take(1)
+                  .toList();
+              return ListView.builder(
+                padding: EdgeInsets.zero,
+                primary: false,
+                shrinkWrap: true,
+                scrollDirection: Axis.vertical,
+                itemCount: codeItems.length,
+                itemBuilder: (context, codeItemsIndex) {
+                  final codeItemsItem = codeItems[codeItemsIndex];
+                  return Text(
+                    'БАЗОВЫЙ ЗАПРОС${codeItemsItem.code} ${codeItemsItem.brand}',
+                    style: FlutterFlowTheme.of(context).bodyMedium,
+                  );
+                },
               );
             },
-          );
-        },
+          ),
+        ],
       ),
     );
   }
