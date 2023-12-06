7131db395a48d1346c2178052233a5c3c07105b5
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2023-12-06 16:18:15 +0000

    Updating to latest FlutterFlow output.

--- lib/components/minprice_model.dart
+++ lib/components/minprice_model.dart
@@ -1,10 +1,13 @@
 import '/backend/schema/structs/index.dart';
+import '/flutter_flow/flutter_flow_icon_button.dart';
 import '/flutter_flow/flutter_flow_theme.dart';
 import '/flutter_flow/flutter_flow_util.dart';
+import '/flutter_flow/custom_functions.dart' as functions;
 import 'minprice_widget.dart' show MinpriceWidget;
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:percent_indicator/percent_indicator.dart';
 import 'package:provider/provider.dart';
 
 class MinpriceModel extends FlutterFlowModel<MinpriceWidget> {
